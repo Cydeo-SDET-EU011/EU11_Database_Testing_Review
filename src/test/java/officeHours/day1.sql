21d212f0af89c19aaa28460ca6e9a26a5559daf2
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-09-07 23:45:55 +0300

    office hour task is added.

---
+++ src/test/java/officeHours/day1.sql
@@ -0,0 +1,33 @@
+-- 1. get me all data from employees table
+-- 2. get me first_name, phone_number and salary from employees
+-- 3. get me unique job_id from _____ table
+-- 4. get me all ST_CLERK from employee table
+-- 5. get me postal_code from JP,UK,CA (solve in two different ways)
+-- 6. get me city not in CH,US
+-- 7. get me job_title whose min_salary is more than 4000 and less than 9000(solve in two different ways)
+-- 8. get me null state_province in locations table
+-- 9. get me manager_id in departments table
+-- 10. get me employee_id and start_date who starts in 2001
+-- 11. get me all employee first_name who works in some CLERK position
+-- 12. get me email whose email is 6 letter and 4th letter of email is E
+-- 13. get me last_name according to their hire_date
+-- 14. get me first_name ascending and salary descending
+-- 15. get me the number of manager_id from departments
+-- 16. get me maximum salary of IT_PROG
+        -- average salary of ST_CLERK
+-- 17. get me all city name uppercase and address losercase
+-- 18. get me every job_title length
+        -- get me firstname + last name
+-- 19. get me maximum salary of each department_id
+-- 20. get me how many employees in each job_id
+-- 21. get me salary information( min, max, sum, avg) of each job_id
+-- 22. get me job_id that total salary is more than 50000
+-- 24. get me which job_id has average commission_pct over 20%
+-- 25. get me each manager_id max salary and find out those more than 10000
+
+
+
+
+
+
+
