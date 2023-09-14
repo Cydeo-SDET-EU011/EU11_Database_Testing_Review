d2f6c565a67ca163646e82f188acb6b1be671d58
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-09-14 20:36:18 +0300

    office hour day2 task is added.

---
+++ src/test/java/officeHours/day2.sql
@@ -0,0 +1,37 @@
+-- 1. get me first 10 result of first_name and job_id.
+-- 2. get me all employees whose salary is lower than average
+-- 3. get me those departments whose employees more than department 80
+-- 4. get me those employees who is older than any IT programmer in the company
+-- 5. get me first name and department name in same table
+-- 6. get me job id and manager id in one table
+-- 7. get me department names and end date( if any employee quit)
+--8. get me job id and job title together
+-- 9. get me first name who left the company
+-- 10. get me first name and job title for those whose salary is equal to max salary of that job  title
+--11.get me department name, city in same table
+--12. get me first name , start date, end date in same table
+
+--13. create a table name laptops, with id, brand, color, price
+--14. create table phones with id, brand, color, price
+
+-- 15. insert laptops table belwo values
+-- 1. (1, 'hp' , 'black', 1500)
+-- 2. (2, 'msi', 'grey', 1300)
+-- 3. (3, 'dell', 'black', 1600)
+-- 4. (4, 'hp', 'white', 1300)
+-- 5. (5, 'macbook', 'white', 1800)
+
+-- 16. insert phones table belwo values
+-- -- 1. (1, 'samsung' , 'black', 700)
+-- -- 2. (2, 'dell', 'grey', 1000)
+-- -- 3. (3, 'msi', 'black', 800)
+-- -- 4. (4, 'iphone', 'white', 1100)
+
+--17. update dell price to 1500
+-- 18. delete macbook from laptops table
+-- 19. combine all brand names in one table( from laptops and phones)
+-- 20. combine unique brand names in one table
+--21. get me unique prices and sort them
+--22. get me common colors for phones and laptops
+--23. get me the colors only laptops have
+--24. get me the brands only phones have
